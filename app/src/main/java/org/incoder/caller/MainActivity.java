a134bed50bbb226bb9a3e40f85be56c41f6d1a04
Jerry xu <Jerry.x@outlook.com>
2018-03-26 12:28:57 +0800

    init project

---
+++ app/src/main/java/org/incoder/caller/MainActivity.java
@@ -0,0 +1,19 @@
+package org.incoder.caller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Caller
+ *
+ * @author Jerry xu
+ * @date 2018/3/25 15:31.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
